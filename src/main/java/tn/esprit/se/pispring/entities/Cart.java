46b352ed43c95b9aab6f86704086b6490c6e3570
haythambrahem <brahem.haytham@esprit.tn>
2024-02-25 16:09:47 +0100

    1.0.7

--- src/main/java/tn/esprit/se/pispring/entities/Cart.java
+++ src/main/java/tn/esprit/se/pispring/entities/Cart.java
@@ -21,8 +21,8 @@ public class Cart {
     private Date datelastitem;
     private Float cart_amount;
     private Integer cart_items_number;
-    @Enumerated(EnumType.STRING)
-    private CartPayment cartPayment;
+
+
     @ManyToOne
     User user;
 
