5e4fe85dfd12213846fd3fd82d2b16cc5e036eed
afef001 <afef.khadraoui@esprit.tn>
2024-04-01 09:44:28 +0200

    sprint1Final

--- src/main/java/tn/esprit/se/pispring/controller/PayrollController.java
+++ src/main/java/tn/esprit/se/pispring/controller/PayrollController.java
@@ -11,9 +11,13 @@ import tn.esprit.se.pispring.Service.PayrollService;
 import tn.esprit.se.pispring.Service.PrimeService;
 import tn.esprit.se.pispring.entities.Payroll;
 import tn.esprit.se.pispring.entities.Prime;
+import tn.esprit.se.pispring.entities.User;
 import tn.esprit.se.pispring.utils.GeneratePdfPayroll;
+import tn.esprit.se.pispring.utils.SendEmail;
 
+import javax.mail.MessagingException;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -25,6 +29,7 @@ public class PayrollController  {
     PayrollService payrollService;
     PrimeService primeService;
     ContributionService contributionService;
+    SendEmail sendEmail;
 
     @GetMapping("/retrieve-all-payrolls")
     public List<Payroll> getPayrolls() {
@@ -49,18 +54,10 @@ public class PayrollController  {
     @RequestMapping(value = "/pdfpayroll/{idpayroll}", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> payrollReport(@PathVariable Long idpayroll) {
-
         var payroll = (Payroll) payrollService.retrievePayroll(idpayroll);
-        var primes = primeService.getListPrimeByUserAndMonth(payroll.getUser(),payroll.getMonth(), payroll.getYear());
-        var contributions = contributionService.getListContributionByUserAndMonth(payroll.getUser(),payroll.getMonth(), payroll.getYear());
-        System.out.println(payroll.toString());
-        GeneratePdfPayroll generatePdfPayroll = new GeneratePdfPayroll();
-
-        ByteArrayInputStream bis = generatePdfPayroll.payrollReport(payroll,primes, contributions);
-
+        ByteArrayInputStream bis = payrollPDFOutput(payroll);
         var headers = new HttpHeaders();
         headers.add("Content-Disposition", "inline; filename=payroll.pdf");
-
         return ResponseEntity
                 .ok()
                 .headers(headers)
@@ -75,6 +72,31 @@ public class PayrollController  {
     @DeleteMapping("/remove-payroll/{id}")
     public void removePayroll(@PathVariable Long id) {
         payrollService.removePayroll(id);
+    }
+
+    @RequestMapping(value = "/send-email/{idpayroll}", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<String> sendEmail(@PathVariable Long idpayroll) throws MessagingException, IOException {
+        var payroll = (Payroll) payrollService.retrievePayroll(idpayroll);
+        ByteArrayInputStream bis = payrollPDFOutput(payroll);
+        String emailBody = "Please find attached your payslip";
+        sendEmail.sendEmailWithAttachment(
+                bis,
+                payroll.getUser().getEmail(),
+                "BULLETIN DE PAIE " +payroll.getMonth()+" "+ payroll.getYear(),
+                emailBody
+        );
+
+        return ResponseEntity
+                .ok("");
+
+    }
+
+    ByteArrayInputStream payrollPDFOutput(Payroll payroll){
+        var primes = primeService.getListPrimeByUserAndMonth(payroll.getUser(),payroll.getMonth(), payroll.getYear());
+        var contributions = contributionService.getListContributionByUserAndMonth(payroll.getUser(),payroll.getMonth(), payroll.getYear());
+        GeneratePdfPayroll generatePdfPayroll = new GeneratePdfPayroll();
+        return generatePdfPayroll.payrollReport(payroll,primes, contributions);
     }
 }
 
