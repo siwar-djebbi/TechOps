e85066cb339d6f8187346593674b3b1f661cf2b5
haythambrahem <brahem.haytham@esprit.tn>
2024-04-02 05:06:33 +0100

    2.0.2

--- src/main/java/tn/esprit/se/pispring/controller/AuthController.java
+++ src/main/java/tn/esprit/se/pispring/controller/AuthController.java
@@ -30,7 +30,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 //@CrossOrigin(origins ="http://localhost:8089")
 @RestController
-//@CrossOrigin
 @RequestMapping("/auth")
 @Slf4j
 @CrossOrigin(origins = "http://localhost:4200")
